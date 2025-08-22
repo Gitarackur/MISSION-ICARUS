5a1589bb50afa268b0d0c868f0307c26b0ed1189
Gitarackur <peterosah5@gmail.com>
2025-08-22 11:35:09 +0200

    update(tree-node)

--- src/ui/components/activity-tree/tree-node.tsx
+++ src/ui/components/activity-tree/tree-node.tsx
@@ -128,8 +128,7 @@ const TreeNode = ({ node, level = 0 }: { node: ActivityTreeNode; level?: number 
               ) : (
                 <div className={`${styles.badgeContainer()} bg-red-100 text-red-700`}>
                   <span className={styles.textLabel()}>
-                    Output:
-                    - {node.activity.outputMatrixId ? JSON.stringify(node.activity.outputMatrixId): "------"}
+                    Output: {node.activity.outputMatrixId ? JSON.stringify(node.activity.outputMatrixId): "------"}
                   </span>
                 </div>
               )}
