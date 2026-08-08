1f3577e05e72d283d8ec2e5a863040b24e9c01ed
Gitarackur <peterosah5@gmail.com>
2026-08-08 12:14:34 +0200

    update

--- src/ui/components/statistics/components/analysis-instructions.tsx
+++ src/ui/components/statistics/components/analysis-instructions.tsx
@@ -1,14 +1,25 @@
-import { ColumnType } from "@/domain/shared/index.types"
-import clsx from "clsx"
+import { tv } from "tailwind-variants"
 
+
+const columnStyles = tv({
+  slots: {
+    string: "rounded-full h-5 w-5 inline-block border-2 border-black bg-yellow-100 dark:bg-yellow-950/30",
+    number: "rounded-full h-5 w-5 inline-block border-2 border-black bg-green-100 dark:bg-green-950/30",
+    boolean: "rounded-full h-5 w-5 inline-block border-2 border-black bg-red-100 dark:bg-red-950/30"
+  },
+  variants: {
+    columnType: {
+      string: "rounded-full h-5 w-5 inline-block border-2 border-black bg-yellow-100 dark:bg-yellow-950/30",
+      number: "rounded-full h-5 w-5 inline-block border-2 border-black bg-green-100 dark:bg-green-950/30",
+      boolean: "rounded-full h-5 w-5 inline-block border-2 border-black bg-red-100 dark:bg-red-950/30"
+    }
+  }
+})
 
 
 const StatisticalAnalysisInstructions = () => {
-  const columnStyles = (value: ColumnType) => clsx("rounded-full h-5 w-5 inline-block border-2 border-black", {
-    "bg-yellow-100 dark:bg-yellow-950/30": value === "string",
-    "bg-green-100 dark:bg-green-950/30": value === "number",
-    "bg-red-100 dark:bg-red-950/30": value === "boolean",
-  })
+  const styles = columnStyles()
+
   return (
     <div>
       <div className="mb-4 p-4 bg-blue-50 rounded-lg">
@@ -17,9 +28,9 @@ const StatisticalAnalysisInstructions = () => {
           <li>• Data Preview page is Paginated.</li>
           <li>• Click on a Menu option to perform Analysis on Columns across <b>ALL</b> pages</li>
           <li className="font-bold flex items-center gap-2">
-            <span className={columnStyles("string")}></span> String Columns 
-            <span className={columnStyles("number")}></span> Numerical Columns 
-            <span className={columnStyles("boolean")}></span> Boolean Columns 
+            <span className={styles.string()}></span> String Columns
+            <span className={styles.number()}></span> Numerical Columns
+            <span className={styles.boolean()}></span> Boolean Columns
           </li>
         </ul>
       </div>
